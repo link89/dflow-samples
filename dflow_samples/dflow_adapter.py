d2bfa8659bf420faccf246e789b018f155df4d60
weihong.xu <xuweihong.cn@qq.com>
2022-11-21 09:09:12 +0800

    wip: simplify dflow adapter

--- dflow_samples/dflow_adapter.py
+++ dflow_samples/dflow_adapter.py
@@ -1,44 +1,21 @@
-from dflow import OutputArtifact, ShellOPTemplate
 from functools import wraps
 import os
 import json
 import traceback
-from typing import Dict, Union, List, TypeVar, Optional, TypedDict
+from typing import Dict, Union, List
 
 DFLOW_DISABLE = '1' == os.getenv('DFLOW_DISABLE', None)
 
 Artifects = Union[Dict[str, Union['Artifects', str, List[str]]], str, List[str]]
 
-T = TypeVar('T')
-class Result(TypedDict):
-    value: Optional[T]
-    artifects: Optional[Artifects]
-
-
-DFLOW_VALUE_FILE = '__dflow_value_file__'
-DFLOW_ERROR_FILE = '__dflow_error_file__'
-DFLOW_ARTIFECTS_FILE = '__dflow_artifects_file__'
-
-
-def dflow_task(fn, prefix='dflow_result'):
+def dflow_task(fn, result_file='argo_result.json', error_file='argo_error.txt'):
     if DFLOW_DISABLE:
         return fn
-
-    value_file = prefix + '_value.json'
-    error_file = prefix + '_error.json'
-    artifects_file = prefix + '_artifects.json'
-
-    setattr(fn, DFLOW_VALUE_FILE, value_file)
-    setattr(fn, DFLOW_ERROR_FILE, error_file)
-    setattr(fn, DFLOW_ARTIFECTS_FILE, artifects_file)
-
     @wraps(fn)
     def wrapped_fn(*args, **kwargs):
         try:
-            ret: Result = fn(*args, **kwargs)
-            if ret is not None:
-                _save_json(ret.value, value_file)
-                _save_json(ret.artifects, artifects_file)
+            ret = fn(*args, **kwargs)
+            _save_json(ret.value, result_file)
             return ret
         except:
             tb = traceback.format_exc()
--- dflow_samples/tasks.py
+++ dflow_samples/tasks.py
@@ -3,14 +3,14 @@ from .dflow_adapter import Result, dflow_task
 
 
 @dflow_task
-def gen_files(n=10, prefix='output-', text='hello world') -> Result:
+def gen_files(n=10, prefix='output-', text='hello world'):
     files = []
     for i in range(n):
         file = prefix + str(i) + '.txt'
         with open(file, 'w') as f:
             f.write(text)
         files.append(file)
-    return Result(artifects=files)
+    return files
 
 
 @dflow_task
